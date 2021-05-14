7ae1322a7d15aaf31bd4c6db5af936427c6f793d
klovae <elyse.klova@gmail.com>
2021-05-14 15:54:33 -0400

    created basic routes, controllers, and plan serializer for plans/index

---
+++ dinner-winner-backend/app/controllers/plans_controller.rb
@@ -0,0 +1,8 @@
+class PlansController < ApplicationController
+
+  def index
+    plans = Plan.all
+    render json: PlanSerializer.new(plans).to_serialized_json
+  end
+
+end
---
+++ dinner-winner-backend/app/services/plan_serializer.rb
@@ -0,0 +1,22 @@
+class PlanSerializer
+  def initialize(plan_object)
+    @plan = plan_object
+  end
+
+  def to_serialized_json
+    options = {
+      include: {
+        tags: {
+          only: [:name]
+        },
+        meals: {
+          only: [:id, :title, :recipe_url, :description, :meal]
+        }
+        
+      }, except: [:created_at, :updated_at]
+    }
+
+    @plan.to_json(options)
+  end
+  
+end
